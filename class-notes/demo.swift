94b72cca71f614188d8a3938ef2c61c9a6362625
Crystal Le <97998586+cryle1205@users.noreply.github.com>
2023-06-09 21:21:07 -0500

    Add files via upload

---
+++ class-notes/demo.swift
@@ -0,0 +1,27 @@
+
+
+
+var a = 4
+var b = 2
+var n = a + b
+
+// what does ... mean or ..<
+var fact = 1
+for i in 1 ... n+1 {
+    fact = fact * i
+    print("factorial of", i, "is", fact)
+}
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+
